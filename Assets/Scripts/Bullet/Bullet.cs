cadbef9848d8ca7bc331c46001b752c0720c1c45
Inheo <inheogame@gmail.com>
2021-08-05 11:18:34 +0300

    Add Pool object

---
+++ Assets/Scripts/Bullet/Bullet.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _bounceForce;
+    [SerializeField] private float _bounceRadius;
+
+    private Vector3 moveDirection;
+
+    private void OnEnable()
+    {
+        moveDirection = transform.forward;
+    }
+
+    private void Update()
+    {
+        transform.Translate(moveDirection * _speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.TryGetComponent(out Block block))
+        {
+            block.Break();
+            gameObject.SetActive(false);
+        }
+        if(other.TryGetComponent(out Obstacle obstacle))
+        {
+            Bounce();
+        }
+    }
+
+    private void Bounce()
+    {
+        moveDirection = (Camera.main.transform.position - transform.position).normalized;
+
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.isKinematic = false;
+        rigidbody.AddExplosionForce(_bounceForce, transform.position + new Vector3(0, -1, -1), _bounceRadius);
+
+        StartCoroutine(Delay(2f, () =>
+        {
+            rigidbody.isKinematic = true;
+            rigidbody.velocity = Vector3.zero;
+            gameObject.SetActive(false);
+        }
+        ));
+    }
+
+    private void OnDisable()
+    {
+        transform.position = Vector3.zero;
+    }
+
+    private IEnumerator Delay(float time, System.Action action)
+    {
+        yield return new WaitForSeconds(time);
+        action?.Invoke();
+    }
+}
